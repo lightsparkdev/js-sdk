689ccb5fa0729a7cf49cef482e1302a97d61cea9
Lightspark Eng <engineering@lightspark.com>
2023-04-11 16:40:31 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 66a298014b079416c5e267d9d600bd279b7a2e32

--- examples/streaming-wallet-extension/src/common/streamingTabs.ts
+++ examples/streaming-wallet-extension/src/common/streamingTabs.ts
@@ -1,15 +1,27 @@
+const URLS = [
+  "https://localhost:3000/demos/streaming",
+  "http://192.168.86.248:3000/demos/streaming",
+  "https://dev.dev.sparkinfra.net/demos/streaming",
+  "https://app.lightspark.com/demos/streaming",
+];
+
 export const findActiveStreamingDemoTabs = () => {
-  return new Promise<chrome.tabs.Tab[]>((resolve) => {
-    chrome.tabs.query(
-      {
-        url: [
-          "https://localhost:3000/demos/streaming",
-          "http://192.168.86.248:3000/demos/streaming",
-          "https://dev.dev.sparkinfra.net/demos/streaming",
-          "https://app.lightspark.com/demos/streaming",
-        ],
-      },
-      (tabs) => resolve(tabs)
-    );
+  const allAndActive = Promise.all([
+    new Promise<chrome.tabs.Tab[]>((resolve) => {
+      chrome.tabs.query({ url: URLS }, (tabs) => resolve(tabs));
+    }),
+    new Promise<chrome.tabs.Tab[]>((resolve) => {
+      chrome.tabs.query(
+        { url: URLS, active: true, lastFocusedWindow: true },
+        (tabs) => resolve(tabs)
+      );
+    }),
+  ]);
+  return allAndActive.then(([all, active]) => {
+    if (active.length > 0) {
+      return active;
+    }
+    // Reverse the order so that the most recently opened tab is returned.
+    return all.reverse();
   });
 };
