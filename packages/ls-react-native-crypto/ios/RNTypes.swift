c0c586d880a4d7d0336c750ab274f1a3487eb434
Jeremy Klein <jklein24@gmail.com>
2023-07-24 11:33:19 -0700

    Add Android support for the RN crypto lib (#5460)
    Needed to switch everything over to base64 strings to go across the JNI
    boundary.
    
    GitOrigin-RevId: 81d6812b643d194b11ea93ec945fec56eaef28f8

--- packages/ls-react-native-crypto/ios/RNTypes.swift
+++ packages/ls-react-native-crypto/ios/RNTypes.swift
@@ -2,10 +2,10 @@ import ExpoModulesCore
 
 internal struct RNMnemonic: Record {
   @Field
-  var mnemonic: String
+  var phrase: String
 }
 
 internal struct RNSeed: Record {
   @Field
-  var seed: [UInt8]
+  var seedBytes: String
 }
---
+++ packages/ls-react-native-crypto/src/base64.ts
@@ -0,0 +1,64 @@
+// Copyright  ©, 2023, Lightspark Group, Inc. - All Rights Reserved
+
+const chars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+const Base64 = {
+  btoa: (input: string = "") => {
+    let str = input;
+    let output = "";
+
+    for (
+      let block = 0, charCode, i = 0, map = chars;
+      str.charAt(i | 0) || ((map = "="), i % 1);
+      output += map.charAt(63 & (block >> (8 - (i % 1) * 8)))
+    ) {
+      charCode = str.charCodeAt((i += 3 / 4));
+
+      if (charCode > 0xff) {
+        throw new Error(
+          "'btoa' failed: The string to be encoded contains characters outside of the Latin1 range."
+        );
+      }
+
+      block = (block << 8) | charCode;
+    }
+
+    return output;
+  },
+
+  atob: (input: string = "") => {
+    let str = input.replace(/=+$/, "");
+    let output = "";
+
+    if (str.length % 4 == 1) {
+      throw new Error(
+        "'atob' failed: The string to be decoded is not correctly encoded."
+      );
+    }
+    for (
+      let bc = 0, bs = 0, buffer, i = 0;
+      (buffer = str.charAt(i++));
+      ~buffer && ((bs = bc % 4 ? bs * 64 + buffer : buffer), bc++ % 4)
+        ? (output += String.fromCharCode(255 & (bs >> ((-2 * bc) & 6))))
+        : 0
+    ) {
+      buffer = chars.indexOf(buffer);
+    }
+
+    return output;
+  },
+};
+
+export const b64decode = (encoded: string): Uint8Array => {
+  return Uint8Array.from(Base64.atob(encoded), (c) => c.charCodeAt(0));
+};
+
+export const urlsafe_b64decode = (encoded: string): Uint8Array => {
+  return b64decode(encoded.replace(/_/g, "/").replace(/-/g, "+"));
+};
+
+export const b64encode = (data: ArrayBuffer): string => {
+  return Base64.btoa(
+    String.fromCharCode.apply(null, Array.from(new Uint8Array(data)))
+  );
+};
