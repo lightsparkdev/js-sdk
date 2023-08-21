456023c3fef3be0617a107b383b5b4c5e067423b
Corey Martin <coreyn.martin@gmail.com>
2023-08-21 11:44:26 -0700

    [js/apps] Experiments service, CreateAccount variants, infer service arg types (#5793)
    Co-authored-by: Lightspark Eng <engineering@lightspark.com>
    GitOrigin-RevId: 70b85ba39787ee89fe0d4fd5426479255a9ddea3

---
+++ packages/ui/types/index.ts
@@ -0,0 +1,12 @@
+declare global {
+  const __CURRENT_COMMIT__: string;
+}
+
+/* window props that may be considered common to any Lightspark web app: */
+export interface LightsparkWindow extends Window {
+  dev?: {
+    [key: string]: unknown;
+  };
+  /* google tag manager. assume always defined, from index.html: */
+  dataLayer: unknown[];
+}
