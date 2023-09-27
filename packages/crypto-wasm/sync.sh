7933a53aa0d5871925256fa1c4cb156303f4ba3f
Zhen Lu <zhenlu@lightspark.com>
2023-09-26 17:58:14 -0700

    Change path in sync.sh to relative path (#6348)
    GitOrigin-RevId: ed0240f460c14f3fb59489295b4502e290db8af3

--- packages/crypto-wasm/sync.sh
+++ packages/crypto-wasm/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/src/lightspark-crypto-uniffi
+cd ../../../../lightspark-crypto-uniffi
 wasm-pack build --target nodejs --out-name crypto
 
 wasm_files[0]="crypto.d.ts"
@@ -9,5 +9,5 @@ wasm_files[2]="crypto_bg.wasm"
 wasm_files[3]="crypto_bg.wasm.d.ts"
 
 for i in ${wasm_files[@]}; do
-    yes | cp -rf ./pkg/$i ~/src/webdev/js/packages/crypto-wasm
+    yes | cp -rf ./pkg/$i ../webdev/js/packages/crypto-wasm/
 done
