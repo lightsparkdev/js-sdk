61b24ec806ab61e0083847a3f04c036dba7c6c8c
Corey Martin <coreyn.martin@gmail.com>
2023-09-27 09:49:31 -0700

    [js] Add explicit type checks to all package build scripts (#6367)
    - tsup seems not to properly type check everything unless it can't build
    - fix offending type error from wallet-sdk
    
    GitOrigin-RevId: 9db792d11c090261b97fbac4054539ae415b6bb1

--- packages/wallet-sdk/src/logger.ts
+++ packages/wallet-sdk/src/logger.ts
@@ -1,9 +1,13 @@
 import { Logger } from "@lightsparkdev/core";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage, {
+  type AsyncStorageStatic,
+} from "@react-native-async-storage/async-storage";
 
 export const logger = new Logger("@lightsparkdev/wallet-sdk", async () => {
   try {
-    const enabled = await AsyncStorage.getItem("lightspark-logging-enabled");
+    const enabled = await (AsyncStorage as any as AsyncStorageStatic).getItem(
+      "lightspark-logging-enabled",
+    );
     return enabled === "1";
   } catch (e) {
     return false;
