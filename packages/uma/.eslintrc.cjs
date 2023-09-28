25203cec9ad3749daf094e305c8d55de9444a34c
Corey Martin <coreyn.martin@gmail.com>
2023-09-27 23:35:41 -0700

    [uma] Fix configs (#6380)
    GitOrigin-RevId: 5ba25aa73a8331357a9aa4cf17d0025f78ddb61e

--- packages/uma/.eslintrc.cjs
+++ packages/uma/.eslintrc.cjs
@@ -4,14 +4,6 @@ module.exports = {
   overrides: [
     {
       files: ["**/*.ts?(x)"],
-      // parserOptions: {
-      //   /* Allow linting for ts files outside src with tsconfig-eslint: */
-      //   project: ["./tsconfig.json", "./tsconfig-eslint.json"],
-      // },
-      rules: {
-        /* Temporarily turn off no-explicit-any until these can be resolved LIG-3400: */
-        "@typescript-eslint/no-explicit-any": "off",
-      },
     },
   ],
 };
