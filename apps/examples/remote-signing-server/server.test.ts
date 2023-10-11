0bc8839490894bc4133b16a3bdfcf2a81397772a
Corey Martin <coreyn.martin@gmail.com>
2023-10-11 16:52:35 -0700

    [remote-signing-server] Handle actual webhook data (#6645)
    GitOrigin-RevId: 5426782d6fd6f84f1d1df5868ed1e73aa763b868

--- apps/examples/remote-signing-server/server.test.ts
+++ apps/examples/remote-signing-server/server.test.ts
@@ -36,7 +36,7 @@ describe("Test server routes", () => {
         "a64c69f1266bc1dc1322c3f40eba7ba2d536c714774a4fc04f0938609482f5d9",
       )
       .send(
-        "7b226576656e745f74797065223a202252454d4f54455f5349474e494e47222c20226576656e745f6964223a20223530353364626438633562303435333439346631633134653031646136396364222c202274696d657374616d70223a2022323032332d30392d31385432333a35303a31352e3335353630332b30303a3030222c2022656e746974795f6964223a20226e6f64655f776974685f7365727665725f7369676e696e673a30313861393633352d333637332d383864662d303030302d383237663233303531623139222c202264617461223a207b227375625f6576656e745f74797065223a202245434448222c2022626974636f696e5f6e6574776f726b223a202252454754455354222c2022706565725f7075626c69635f6b6579223a2022303331373364393764303937336435393637313663386364313430363665323065323766363836366162323134666430346431363033303136313564653738663732227d7d",
+        `{"event_type": "REMOTE_SIGNING", "event_id": "5053dbd8c5b0453494f1c14e01da69cd", "timestamp": "2023-09-18T23:50:15.355603+00:00", "entity_id": "node_with_server_signing:018a9635-3673-88df-0000-827f23051b19", "data": {"sub_event_type": "ECDH", "bitcoin_network": "REGTEST", "peer_public_key": "03173d97d0973d596716c8cd14066e20e27f6866ab214fd04d160301615de78f72"}}`,
       );
 
     expect(response.status).toBe(200);
@@ -44,18 +44,16 @@ describe("Test server routes", () => {
 
   test("error reponse from /lightspark-webhook webhook data is invalid", async () => {
     let response: supertest.Response;
-    try {
-      jest
-        .spyOn(LightsparkClient.prototype, "executeRawQuery")
-        .mockReturnValue(Promise.resolve("mock-query-response"));
-      response = await request
-        .post("/lightspark-webhook")
-        .set(
-          "lightspark-signature",
-          "a64c69f1266bc1dc1322c3f40eba7ba2d536c714774a4fc04f0938609482f5d9",
-        )
-        .send("7b226576656e");
-      expect(response.status).toBe(500);
-    } catch (error) {}
+    jest
+      .spyOn(LightsparkClient.prototype, "executeRawQuery")
+      .mockReturnValue(Promise.resolve("mock-query-response"));
+    response = await request
+      .post("/lightspark-webhook")
+      .set(
+        "lightspark-signature",
+        "a64c69f1266bc1dc1322c3f40eba7ba2d536c714774a4fc04f0938609482f5d9",
+      )
+      .send("7b226576656e");
+    expect(response.status).toBe(500);
   });
 });
--- apps/examples/remote-signing-server/src/index.ts
+++ apps/examples/remote-signing-server/src/index.ts
@@ -3,7 +3,10 @@ import {
   AccountTokenAuthProvider,
   LightsparkClient,
   RemoteSigningWebhookHandler,
+  verifyAndParseWebhook,
   WebhookEvent,
+  WebhookEventType,
+  WEBHOOKS_SIGNATURE_HEADER,
 } from "@lightsparkdev/lightspark-sdk";
 import {
   EnvCredentials,
@@ -17,61 +20,68 @@ export const app = express();
 const WEBHOOK_SECRET = process.env.RK_WEBHOOK_SECRET;
 const MASTER_SEED_HEX = process.env.RK_MASTER_SEED_HEX;
 
-app.use(bodyParser.text({ type: "*/*" })); // Middleware to parse raw body
+app.use(bodyParser.raw({ type: "*/*" })); // Middleware to parse raw body
 
 app.get("/ping", (req, res) => {
   console.log("ping");
   res.send("OK");
 });
 
-app.post("/lightspark-webhook", (req, res) => {
+app.post("/lightspark-webhook", async (req, res) => {
   let credentials: EnvCredentials;
   try {
-    try {
-      credentials = getCredentialsFromEnvOrThrow();
-    } catch (e) {
-      res.status(500).send("Unable to get credentials from env");
-      return;
-    }
-
-    const lightsparkClient = new LightsparkClient(
-      new AccountTokenAuthProvider(
-        credentials.apiTokenClientId,
-        credentials.apiTokenClientSecret,
-      ),
-      credentials.baseUrl,
-    );
-
-    const validator = {
-      should_sign: (webhook: WebhookEvent) => true,
-    };
-    const remoteSigningHandler = new RemoteSigningWebhookHandler(
-      lightsparkClient,
-      hexToBytes(MASTER_SEED_HEX),
-      validator,
-    );
-
-    const signatureHeader = req.headers["lightspark-signature"];
-    console.log("signatureHeader", signatureHeader);
+    const signatureHeader = req.headers[WEBHOOKS_SIGNATURE_HEADER];
 
     if (typeof signatureHeader !== "string") {
-      console.error("Invalid signature header");
       res.status(400).send("Invalid signature header");
       return;
     }
 
-    console.log("body", req.body);
-
-    const result = remoteSigningHandler.handleWebhookRequest(
-      hexToBytes(req.body),
+    const webhook = await verifyAndParseWebhook(
+      req.body,
       signatureHeader,
       WEBHOOK_SECRET,
     );
-    console.log("result", result);
+
+    switch (webhook.event_type) {
+      case WebhookEventType.REMOTE_SIGNING:
+        try {
+          credentials = getCredentialsFromEnvOrThrow();
+        } catch (e) {
+          res.status(500).send("Unable to get credentials from env");
+          return;
+        }
+
+        const lightsparkClient = new LightsparkClient(
+          new AccountTokenAuthProvider(
+            credentials.apiTokenClientId,
+            credentials.apiTokenClientSecret,
+          ),
+          credentials.baseUrl,
+        );
+
+        const validator = {
+          should_sign: (webhook: WebhookEvent) => true,
+        };
+
+        const remoteSigningHandler = new RemoteSigningWebhookHandler(
+          lightsparkClient,
+          hexToBytes(MASTER_SEED_HEX),
+          validator,
+        );
+
+        const result = remoteSigningHandler.handleWebhookRequest(
+          req.body,
+          signatureHeader,
+          WEBHOOK_SECRET,
+        );
+        break;
+      default:
+        res.status(400).send("Unknown webhook type");
+    }
 
     res.send("OK");
   } catch (e) {
-    console.error(e);
     res.status(500).send("Internal server error");
   }
 });
