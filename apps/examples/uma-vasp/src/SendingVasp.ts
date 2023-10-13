81e866732f301ec5a5fbed83125c771cf30d7ee8
Jeremy Klein <jklein24@gmail.com>
2023-10-13 14:16:50 -0700

    Implement payReq request for uma JS VASP (#6846)
    GitOrigin-RevId: 4e28858eb367509f5f6aede93d4d5ef175aad4cb

--- apps/examples/uma-vasp/src/SendingVasp.ts
+++ apps/examples/uma-vasp/src/SendingVasp.ts
@@ -1,7 +1,8 @@
 import { hexToBytes } from "@lightsparkdev/core";
-import { LightsparkClient } from "@lightsparkdev/lightspark-sdk";
+import { InvoiceData, LightsparkClient } from "@lightsparkdev/lightspark-sdk";
 import * as uma from "@uma-sdk/core";
 import { Express, Request, Response } from "express";
+import settings from "../../settings.json" assert { type: "json" };
 import SendingVaspRequestCache from "./SendingVaspRequestCache.js";
 import UmaConfig from "./UmaConfig.js";
 
@@ -72,17 +73,8 @@ export default class SendingVasp {
       return;
     }
 
-    let pubKeys: uma.PubKeyResponse;
-    try {
-      pubKeys = await uma.fetchPublicKeyForVasp({
-        cache: this.pubKeyCache,
-        vaspDomain: receivingVaspDomain,
-      });
-    } catch (e) {
-      console.error("Error fetching public key.", e);
-      res.status(424).send("Error fetching public key.");
-      return;
-    }
+    let pubKeys = await this.fetchPubKeysOrFail(receivingVaspDomain, res);
+    if (!pubKeys) return;
 
     try {
       const isSignatureValid = await uma.verifyUmaLnurlpResponseSignature(
@@ -117,13 +109,172 @@ export default class SendingVasp {
 
   private async handleClientUmaPayreq(req: Request, res: Response) {
     const callbackUuid = req.params.callbackUuid;
-    // const payreq = await this.payreq(callbackUuid);
-    res.send("ok");
+    if (!callbackUuid) {
+      res.status(400).send("Missing callbackUuid");
+      return;
+    }
+
+    const initialRequestData =
+      this.requestCache.getLnurlpResponseData(callbackUuid);
+    if (!initialRequestData) {
+      res.status(400).send("callbackUuid not found");
+      return;
+    }
+
+    const amountStr = req.query.amount;
+    if (!amountStr || typeof amountStr !== "string") {
+      res.status(400).send("Missing amount");
+      return;
+    }
+    const amount = parseInt(amountStr);
+    if (isNaN(amount)) {
+      res.status(400).send("Invalid amount");
+      return;
+    }
+
+    const currencyCode = req.query.currencyCode;
+    if (!currencyCode || typeof currencyCode !== "string") {
+      res.status(400).send("Missing currencyCode");
+      return;
+    }
+    const currencyValid = initialRequestData.lnurlpResponse.currencies.some(
+      (c) => c.code === currencyCode,
+    );
+    if (!currencyValid) {
+      res.status(400).send("Currency code not supported");
+      return;
+    }
+
+    let pubKeys = await this.fetchPubKeysOrFail(
+      initialRequestData.receivingVaspDomain,
+      res,
+    );
+    if (!pubKeys) return;
+
+    const payerProfile = this.getPayerProfile(
+      initialRequestData.lnurlpResponse.requiredPayerData,
+    );
+    const trInfo =
+      '["message": "Here is some fake travel rule info. It is up to you to actually implement this if needed."]';
+    // In practice this should be loaded from your node:
+    const payerUtxos: string[] = [];
+    const utxoCallback = this.getUtxoCallback(req, "1234abcd");
+
+    let payReq: uma.PayRequest;
+    try {
+      payReq = await uma.getPayRequest({
+        receiverEncryptionPubKey: hexToBytes(pubKeys.encryptionPubKey),
+        sendingVaspPrivateKey: this.config.umaSigningPrivKey(),
+        currencyCode,
+        amount,
+        payerIdentifier: payerProfile.identifier,
+        payerKycStatus: uma.KycStatus.Verified,
+        utxoCallback,
+        trInfo,
+        payerUtxos,
+        payerName: payerProfile.name,
+        payerEmail: payerProfile.email,
+      });
+    } catch (e) {
+      console.error("Error generating payreq.", e);
+      res.status(500).send("Error generating payreq.");
+      return;
+    }
+
+    let response: globalThis.Response;
+    try {
+      response = await fetch(initialRequestData.lnurlpResponse.callback, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payReq),
+      });
+    } catch (e) {
+      res.status(500).send("Error sending payreq.");
+      return;
+    }
+
+    if (!response.ok) {
+      res.status(424).send(`Payreq failed. ${response.status}`);
+      return;
+    }
+
+    let payResponse: uma.PayReqResponse;
+    try {
+      payResponse = await uma.parsePayReqResponse(await response.text());
+    } catch (e) {
+      console.error("Error parsing payreq response.", e);
+      res.status(424).send("Error parsing payreq response.");
+      return;
+    }
+
+    // This is where you'd pre-screen the UTXOs from payResponse.compliance.utxos.
+
+    let invoice: InvoiceData;
+    try {
+      invoice = await this.lightsparkClient.decodeInvoice(
+        payResponse.encodedInvoice,
+      );
+    } catch (e) {
+      console.error("Error decoding invoice.", e);
+      res.status(500).send("Error decoding invoice.");
+      return;
+    }
+
+    const newCallbackUuid = this.requestCache.savePayReqData(
+      payResponse.encodedInvoice,
+      utxoCallback,
+      invoice,
+    );
+
+    res.send({
+      callbackUuid: newCallbackUuid,
+      encodedInvoice: payResponse.encodedInvoice,
+      amount: invoice.amount,
+      conversionRate: payResponse.paymentInfo.multiplier,
+      exchangeFeesMillisatoshi:
+        payResponse.paymentInfo.exchangeFeesMillisatoshi,
+      currencyCode: payResponse.paymentInfo.currencyCode,
+    });
+  }
+
+  private async fetchPubKeysOrFail(receivingVaspDomain: string, res: Response) {
+    try {
+      return await uma.fetchPublicKeyForVasp({
+        cache: this.pubKeyCache,
+        vaspDomain: receivingVaspDomain,
+      });
+    } catch (e) {
+      console.error("Error fetching public key.", e);
+      res.status(424).send("Error fetching public key.");
+    }
   }
 
   private async handleClientSendPayment(req: Request, res: Response) {
     const callbackUuid = req.params.callbackUuid;
     // const payment = await this.sendPayment(callbackUuid);
     res.send("ok");
+  }
+
+  /**
+   * NOTE: In a real application, you'd want to use the authentication context to pull out this information. It's not
+   * actually always Alice sending the money ;-).
+   */
+  private getPayerProfile(requiredPayerData: uma.PayerDataOptions) {
+    const port = process.env.PORT || settings.umaVasp.port;
+    return {
+      name: requiredPayerData.nameRequired ? "Alice FakeName" : undefined,
+      email: requiredPayerData.emailRequired ? "alice@vasp1.com" : undefined,
+      // Note: This is making an assumption that this is running on localhost. We should make it configurable.
+      identifier: `$alice@localhost:${port}`,
+    };
+  }
+
+  private getUtxoCallback(req: Request, txId: string): string {
+    const protocol = req.protocol;
+    const host = req.hostname;
+    const path = `/api/uma/utxoCallback?txId=${txId}`;
+    return `${protocol}://${host}${path}`;
   }
 }
