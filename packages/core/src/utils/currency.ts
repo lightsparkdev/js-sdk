7b8345e216aec76beac4b1265bcdca508869bda0
Corey Martin <coreyn.martin@gmail.com>
2023-10-17 16:01:06 -0700

    [core] Use core round instead (#6955)
    GitOrigin-RevId: b0c0fd4f29b0f871afff4412524e83868d5b88a9

--- packages/core/src/utils/currency.ts
+++ packages/core/src/utils/currency.ts
@@ -1,6 +1,7 @@
 // Copyright ©, 2023-present, Lightspark Group, Inc. - All Rights Reserved
 
 import LightsparkException from "../LightsparkException.js";
+import { round } from "./numbers.js";
 
 /** Represents the value and unit for an amount of currency. **/
 type CurrencyAmount = {
@@ -58,15 +59,15 @@ const CONVERSION_MAP = {
     [CurrencyUnit.SATOSHI]: (v: number) => v * 100_000_000,
   },
   [CurrencyUnit.MICROBITCOIN]: {
-    [CurrencyUnit.BITCOIN]: (v: number) => Math.round(v / 1_000_000),
+    [CurrencyUnit.BITCOIN]: (v: number) => round(v / 1_000_000),
     [CurrencyUnit.MICROBITCOIN]: (v: number) => v,
-    [CurrencyUnit.MILLIBITCOIN]: (v: number) => Math.round(v / 1000),
+    [CurrencyUnit.MILLIBITCOIN]: (v: number) => round(v / 1000),
     [CurrencyUnit.MILLISATOSHI]: (v: number) => v * 100_000,
     [CurrencyUnit.NANOBITCOIN]: (v: number) => v * 1000,
     [CurrencyUnit.SATOSHI]: (v: number) => v * 100,
   },
   [CurrencyUnit.MILLIBITCOIN]: {
-    [CurrencyUnit.BITCOIN]: (v: number) => Math.round(v / 1_000),
+    [CurrencyUnit.BITCOIN]: (v: number) => round(v / 1_000),
     [CurrencyUnit.MICROBITCOIN]: (v: number) => v * 1000,
     [CurrencyUnit.MILLIBITCOIN]: (v: number) => v,
     [CurrencyUnit.MILLISATOSHI]: (v: number) => v * 100_000_000,
@@ -74,25 +75,25 @@ const CONVERSION_MAP = {
     [CurrencyUnit.SATOSHI]: (v: number) => v * 100_000,
   },
   [CurrencyUnit.MILLISATOSHI]: {
-    [CurrencyUnit.BITCOIN]: (v: number) => Math.round(v / 100_000_000_000),
-    [CurrencyUnit.MICROBITCOIN]: (v: number) => Math.round(v / 100_000),
-    [CurrencyUnit.MILLIBITCOIN]: (v: number) => Math.round(v / 100_000_000),
+    [CurrencyUnit.BITCOIN]: (v: number) => round(v / 100_000_000_000),
+    [CurrencyUnit.MICROBITCOIN]: (v: number) => round(v / 100_000),
+    [CurrencyUnit.MILLIBITCOIN]: (v: number) => round(v / 100_000_000),
     [CurrencyUnit.MILLISATOSHI]: (v: number) => v,
-    [CurrencyUnit.NANOBITCOIN]: (v: number) => Math.round(v / 100),
-    [CurrencyUnit.SATOSHI]: (v: number) => Math.round(v / 1000),
+    [CurrencyUnit.NANOBITCOIN]: (v: number) => round(v / 100),
+    [CurrencyUnit.SATOSHI]: (v: number) => round(v / 1000),
   },
   [CurrencyUnit.NANOBITCOIN]: {
-    [CurrencyUnit.BITCOIN]: (v: number) => Math.round(v / 1_000_000_000),
-    [CurrencyUnit.MICROBITCOIN]: (v: number) => Math.round(v / 1000),
-    [CurrencyUnit.MILLIBITCOIN]: (v: number) => Math.round(v / 1_000_000),
+    [CurrencyUnit.BITCOIN]: (v: number) => round(v / 1_000_000_000),
+    [CurrencyUnit.MICROBITCOIN]: (v: number) => round(v / 1000),
+    [CurrencyUnit.MILLIBITCOIN]: (v: number) => round(v / 1_000_000),
     [CurrencyUnit.MILLISATOSHI]: (v: number) => v * 100,
     [CurrencyUnit.NANOBITCOIN]: (v: number) => v,
-    [CurrencyUnit.SATOSHI]: (v: number) => Math.round(v / 10),
+    [CurrencyUnit.SATOSHI]: (v: number) => round(v / 10),
   },
   [CurrencyUnit.SATOSHI]: {
-    [CurrencyUnit.BITCOIN]: (v: number) => Math.round(v / 100_000_000),
-    [CurrencyUnit.MICROBITCOIN]: (v: number) => Math.round(v / 100),
-    [CurrencyUnit.MILLIBITCOIN]: (v: number) => Math.round(v / 100_000),
+    [CurrencyUnit.BITCOIN]: (v: number) => round(v / 100_000_000),
+    [CurrencyUnit.MICROBITCOIN]: (v: number) => round(v / 100),
+    [CurrencyUnit.MILLIBITCOIN]: (v: number) => round(v / 100_000),
     [CurrencyUnit.MILLISATOSHI]: (v: number) => v * 1000,
     [CurrencyUnit.NANOBITCOIN]: (v: number) => v * 10,
     [CurrencyUnit.SATOSHI]: (v: number) => v,
