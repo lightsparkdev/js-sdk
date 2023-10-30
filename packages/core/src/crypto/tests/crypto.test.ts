e9ebaae5758c6850f5d8eaac7e5111dd310a143c
Sorizen <anderteiker505@gmail.com>
2023-10-30 20:10:38 +0200

    fixes before main merge

---
+++ packages/core/src/crypto/tests/crypto.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from '@jest/globals'
+import { b64encode, DefaultCrypto } from '../../../dist/index.js'
+
+describe('Crypto tests', () => {
+    test(
+        'should generate a key',
+        async () => {
+            const { privateKey, publicKey,  } =
+                await DefaultCrypto.generateSigningKeyPair()
+
+            const serializedKeypair = {
+                privateKey: b64encode(
+                    await DefaultCrypto.serializeSigningKey(privateKey, 'pkcs8'),
+                ),
+                publicKey: b64encode(
+                    await DefaultCrypto.serializeSigningKey(publicKey, 'spki'),
+                ),
+            }
+
+            expect(serializedKeypair.privateKey).not.toBeNull()
+            expect(serializedKeypair.publicKey).not.toBeNull()
+        },
+    )
+})
