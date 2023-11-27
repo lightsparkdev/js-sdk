7fe546764913a82f49425a70249019df559bf6e4
Corey Martin <coreyn.martin@gmail.com>
2023-11-27 10:25:41 -0800

    [site] Wrapper for Stripe errors and better logging (#7720)
    GitOrigin-RevId: 112d124e7d2c8947d8d91c309fa5f2ae40639a1f

--- packages/core/src/utils/types.ts
+++ packages/core/src/utils/types.ts
@@ -27,3 +27,7 @@ export type DeepPartial<T> = T extends object
       [P in keyof T]?: DeepPartial<T[P]>;
     }
   : T;
+
+export type JSONLiteral = string | number | boolean | null;
+export type JSONType = JSONLiteral | JSONType[] | { [key: string]: JSONType };
+export type JSONObject = { [key: string]: JSONType };
