f7722064b934ce565848b16c461793517b1f0763
Brian Siao Tick Chong <bsiaotickchong@gmail.com>
2023-12-05 14:04:39 -0800

    [jwt-server] refactor to use express, ngrok, and level instead of firebase (towards LIG-3502, LIG-3490) (#7847)
    Instructions for running this are in README
    
    - uses express to server endpoint locally
    - uses ngrok to make endpoint publicly available (so that this will work with our mobile wallet demos)
    - uses level instead of firebase as a db
    - uses scrypt with a password salt for better security standards
    
    GitOrigin-RevId: aa0193ee6f48fe00fbda4f8ae15c2904bc85579b

--- apps/examples/jwt-server/src/index.ts
+++ apps/examples/jwt-server/src/index.ts
@@ -1,112 +1,95 @@
-import {
-  verifyAndParseWebhook,
-  WEBHOOKS_SIGNATURE_HEADER,
-} from "@lightsparkdev/lightspark-sdk";
-import { createHash } from "crypto";
-import { initializeApp } from "firebase-admin/app";
-import * as firestore from "firebase-admin/firestore";
-import { defineSecret, defineString } from "firebase-functions/params";
-import { logger } from "firebase-functions/v2";
-import { onRequest } from "firebase-functions/v2/https";
 import jwt from "jsonwebtoken";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { Level } from "level";
+import * as crypto from "crypto";
 
-initializeApp();
+const env = dotenv.config({
+  path: process.env.HOME + "/.lightsparkenv",
+}).parsed || {};
+const accountId = env["LIGHTSPARK_ACCOUNT_ID"];
+const privateSigningKey = env["LIGHTSPARK_JWT_PRIV_KEY"];
 
-const accountIdParam = defineString("ACCOUNT_ID");
-const privateSigningKeyParam = defineSecret("ACCOUNT_PRIVATE_SIGNING_KEY");
-const webhookSecretParam = defineSecret("WEBHOOK_SECRET");
+if (!accountId || !privateSigningKey) {
+  throw new Error(
+    "Missing account config. Please run lightspark-wallet init-env to configure your account locally." +
+    "See: https://docs.lightspark.com/wallet-sdk/wallet-cli",
+  );
+}
 
-export const getJwt = onRequest(
-  { cors: true, secrets: [privateSigningKeyParam] },
-  async (request, response) => {
-    const userId = request.query.userId as string;
-    const password = request.query.password as string;
-    response.set("Access-Control-Allow-Origin", "*");
-    if (!userId || !password) {
-      response.status(400).send("Missing userId or password");
-      return;
-    }
+const db = new Level("./mydb", { valueEncoding: "json" });
 
-    const isUserValid = await checkOrCreateUser(userId, password);
-    if (!isUserValid) {
-      response.status(401).send("Invalid userId+password combo");
-      return;
-    }
+const app = express();
+app.use(cors());
 
-    const accountId = accountIdParam.value();
-    const privateSigningKey = privateSigningKeyParam.value();
-    if (!accountId || !privateSigningKey) {
-      response
-        .status(500)
-        .send(
-          "Missing account config. Please add account_id and " +
-            "private_signing_key to the firebase config."
-        );
-      return;
-    }
+app.get("/getJwt", (req, res) => {
+  handleGetJwt(req, res).catch((err) => {
+    res.status(500).send(err);
+  });
+});
 
-    const claims = {
-      aud: "https://api.lightspark.com",
-      // Any unique identifier for the user.
-      sub: userId,
-      // True to use the test environment, false to use production.
-      test: true,
-      iat: Math.floor(Date.now() / 1000),
-      // Expriation time for the JWT is 30 days from now.
-      exp: Math.floor((Date.now() + 1000 * 60 * 60 * 24 * 30) / 1000),
-    };
-    console.log("claims", claims);
+app.listen(3000);
 
-    console.log(`jwt: ${JSON.stringify(Object.entries(jwt))}`);
-    const token = jwt.sign(claims, privateSigningKey, { algorithm: "ES256" });
-
-    logger.info("Generating JWT for a user", { userId, accountId });
-    response.send({ token, accountId });
+const handleGetJwt = async (req: express.Request, res: express.Response) => {
+  const userId = req.query.userId as string;
+  const password = req.query.password as string;
+  res.set("Access-Control-Allow-Origin", "*");
+  if (!userId || !password) {
+    res.status(400).send("Missing userId or password");
+    return;
   }
-);
 
-export const handleWebhook = onRequest(
-  { cors: true, secrets: [webhookSecretParam] },
-  async (request, response) => {
-    const hexdigest = request.header(WEBHOOKS_SIGNATURE_HEADER) || "";
-    const bodyData = new TextEncoder().encode(JSON.stringify(request.body));
-    const event = await verifyAndParseWebhook(
-      bodyData,
-      hexdigest,
-      webhookSecretParam.value()
-    );
-    console.log(`Received webhook event: ${event.event_type}`);
-    console.log(`Event data: ${JSON.stringify(event)}`);
-
-    // From here, you could take some action based on the event type. For
-    // example: if (event.event_type ===
-    // WebhookEventType.WALLET_INCOMING_PAYMENT_FINISHED) { const payment =
-    // await lightsparkClient.executeRawQuery(
-    // IncomingPayment.getIncomingPaymentQuery(event.entity_id) );
-    // console.log(`Incoming payment: ${JSON.stringify(payment)}`);
-    // const wallet = await
-    // lightsparkClient.executeRawQuery(Wallet.getWalletQuery(payment.wallet_id));
-    // await sendPaymentReceivedNotification(wallet.thirdPartyIdentifier,
-    // payment.amount); }
-
-    response.send("ok");
+  const isUserValid = await checkOrCreateUser(userId, password);
+  if (!isUserValid) {
+    res.status(401).send("Invalid userId+password combo");
+    return;
   }
-);
+
+  const claims = {
+    aud: "https://api.lightspark.com",
+    // Any unique identifier for the user.
+    sub: userId,
+    // True to use the test environment, false to use production.
+    test: true,
+    iat: Math.floor(Date.now() / 1000),
+    // Expiration time for the JWT is 30 days from now.
+    exp: Math.floor((Date.now() + 1000 * 60 * 60 * 24 * 30) / 1000),
+  };
+  console.log("claims", claims);
+
+  console.log(`jwt: ${JSON.stringify(Object.entries(jwt))}`);
+  const token = jwt.sign(claims, privateSigningKey, { algorithm: "ES256" });
+
+  console.log("Generating JWT for a user", { userId, accountId });
+  res.send({ token, accountId });
+};
 
 const checkOrCreateUser = async (userId: string, password: string) => {
-  const hash = createHash("sha256");
-  const hashedPassword = hash.update(password).digest("base64");
-  const existingUser = await firestore
-    .getFirestore()
-    .doc(`users/${userId}`)
-    .get();
-  if (existingUser.exists) {
-    const userData = existingUser.data();
-    return userData?.passwordHash === hashedPassword;
+  let existingUser;
+  try {
+    existingUser = await db.get(userId);
+  } catch (err) {
+    if ((err as { notFound: boolean }).notFound) {
+      console.log("err", err);
+      // Do nothing, this is expected if the user doesn't exist yet.
+    } else {
+      throw err;
+    }
   }
 
-  await firestore.getFirestore().doc(`users/${userId}`).set({
-    passwordHash: hashedPassword,
-  });
+  // If the user already exists, check that the derived key matches.
+  if (existingUser) {
+    const userData = JSON.parse(existingUser) as { key: string; salt: string };
+    const key = crypto.scryptSync(password, userData.salt, 64).toString("base64");
+    console.log("Existing user!", userId);
+    return userData.key === key;
+  }
+
+  // Otherwise create a new user with a random salt and a key derived from the password.
+  const salt = crypto.randomBytes(16).toString("base64");
+  const key = crypto.scryptSync(password, salt, 64).toString("base64");
+  console.log("New user!", userId);
+  await db.put(userId, JSON.stringify({ key, salt }));
   return true;
 };
