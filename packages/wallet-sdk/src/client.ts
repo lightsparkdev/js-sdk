4e559c821603e4c51254599d7f23bd30be60136f
Jeremy Klein <jklein24@gmail.com>
2023-12-04 22:22:24 -0800

    format again
    GitOrigin-RevId: adbb9a80b185b593d875df85bb1b1a898f3e49a7

--- packages/wallet-sdk/src/client.ts
+++ packages/wallet-sdk/src/client.ts
@@ -68,9 +68,7 @@ import WalletStatus from "./objects/WalletStatus.js";
 import { WalletToPaymentRequestsConnectionFromJson } from "./objects/WalletToPaymentRequestsConnection.js";
 import { WalletToTransactionsConnectionFromJson } from "./objects/WalletToTransactionsConnection.js";
 import type WithdrawalFeeEstimateOutput from "./objects/WithdrawalFeeEstimateOutput.js";
-import {
-  WithdrawalFeeEstimateOutputFromJson,
-} from "./objects/WithdrawalFeeEstimateOutput.js";
+import { WithdrawalFeeEstimateOutputFromJson } from "./objects/WithdrawalFeeEstimateOutput.js";
 import type WithdrawalMode from "./objects/WithdrawalMode.js";
 import type WithdrawalRequest from "./objects/WithdrawalRequest.js";
 import { WithdrawalRequestFromJson } from "./objects/WithdrawalRequest.js";
