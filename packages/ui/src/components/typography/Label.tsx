abedcb7076c016f290872c6263601c4249174ef9
Corey Martin <coreyn.martin@gmail.com>
2024-05-08 15:56:54 -0700

    [ui] Convert Label (#10436)
    GitOrigin-RevId: fa2041f1aef33e3e149b7460d5a61253a3b6dce8

--- packages/ui/src/components/typography/Label.tsx
+++ packages/ui/src/components/typography/Label.tsx
@@ -1,22 +1,38 @@
 "use client";
 
 import styled from "@emotion/styled";
+import { type ReactNode } from "react";
 import { getFontColor, type FontColorKey } from "../../styles/themes.js";
 import {
   getTypographyString,
   type TokenSizeKey,
 } from "../../styles/tokens/typography.js";
+import {
+  toNonTypographicReactNodes,
+  type ToNonTypographicReactNodesArgs,
+} from "../../utils/toNonTypographicReactNodes.js";
 
 type LabelProps = {
-  children: React.ReactNode;
+  content?: ToNonTypographicReactNodesArgs | undefined | null;
+  /* children must be a string. use content prop for more complex content */
+  children?: string | undefined | null;
   size?: TokenSizeKey;
   color?: FontColorKey | undefined;
 };
 
-export const Label = ({ children, color, size = "Medium" }: LabelProps) => {
+export const Label = ({
+  content,
+  color,
+  size = "Medium",
+  children,
+}: LabelProps) => {
+  let reactNodes: ReactNode = children || null;
+  if (content) {
+    reactNodes = toNonTypographicReactNodes(content);
+  }
   return (
     <StyledLabel size={size} colorProp={color}>
-      {children}
+      {reactNodes}
     </StyledLabel>
   );
 };
