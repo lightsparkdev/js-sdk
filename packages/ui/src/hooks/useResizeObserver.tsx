655416d4d18a75b2e751685694ecdf8d012d249e
Corey Martin <coreyn.martin@gmail.com>
2024-05-17 13:46:38 -0700

    [site] Ignore ResizeObserver errors in Cypress (#10701)
    GitOrigin-RevId: c565e89edf49b23bea84930292176b1fa2905405

--- packages/ui/src/hooks/useResizeObserver.tsx
+++ packages/ui/src/hooks/useResizeObserver.tsx
@@ -50,12 +50,7 @@ export function useResizeObserver<K extends keyof ResizeProperties>(
         resizeProperties &&
         resizeProperties.length > 0
       ) {
-        resizeObserver.current = new ResizeObserver((entries) => {
-          /* See https://mzl.la/3WEF8m1 and https://bit.ly/3UHS9IS for explanation
-             ResizeObserver was sometimes throwing an uncaught error in Cypress
-             `ResizeObserver loop completed with undelivered notifications` */
-          setTimeout(() => onResize(entries), 0);
-        });
+        resizeObserver.current = new ResizeObserver(onResize);
         resizeObserver.current.observe(node);
       }
     },
