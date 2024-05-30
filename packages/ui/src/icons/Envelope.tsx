2c4143b9da35079ea8d8a4d7d7a6983d4c1c5146
Corey Martin <coreyn.martin@gmail.com>
2024-05-29 17:58:12 -0700

    [uma-bridge] Card header styles (#10842)
    GitOrigin-RevId: d3d3e9eabbea9881e602f968f102e1f6399b8a77

---
+++ packages/ui/src/icons/Envelope.tsx
@@ -0,0 +1,20 @@
+// Copyright  ©, 2022, Lightspark Group, Inc. - All Rights Reserved
+
+export function Envelope() {
+  return (
+    <svg
+      width="100%"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M35.0027 15.417C30.8238 18.5565 25.6293 20.417 20.0002 20.417C14.371 20.417 9.17645 18.5565 4.99773 15.417M4.5835 7.91699H35.4168V32.0837H4.5835V7.91699Z"
+        stroke="currentColor"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
