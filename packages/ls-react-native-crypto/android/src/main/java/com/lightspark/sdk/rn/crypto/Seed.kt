48727296935da58a1312d051ee424c2e77890da7
Corey Martin <coreyn.martin@gmail.com>
2024-09-24 17:29:29 -0700

    [ls-react-native-crypto] Remove package (#12567)
    GitOrigin-RevId: 7c20c886ea9b96036e8fd6abb1f94a45186fd4ab

--- packages/ls-react-native-crypto/android/src/main/java/com/lightspark/sdk/rn/crypto/Seed.kt
+++
@@ -1,9 +0,0 @@
-package com.lightspark.sdk.rn.crypto
-
-import expo.modules.kotlin.records.Field
-import expo.modules.kotlin.records.Record
-
-class Seed(
-    @Field
-    val seedBytes: String = "",
-) : Record
