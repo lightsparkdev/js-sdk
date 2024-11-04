880d7ed4585f4e8ec922fb1728fe9ceca8ac895f
Corey Martin <coreyn.martin@gmail.com>
2024-11-04 13:56:46 -0800

    [ui] Improved CommaNumberInput (#13187)
    GitOrigin-RevId: 7aaedd8deda222983fb7a8c42ab69e6e39e6ea51

---
+++ packages/ui/src/hooks/useNumberInput/utils/removeInvalidChars.ts
@@ -0,0 +1,13 @@
+import { escapeRegExp } from "./escapeRegExp.js";
+
+/**
+ * Remove invalid characters
+ */
+export const removeInvalidChars = (
+  value: string,
+  validChars: ReadonlyArray<string>,
+): string => {
+  const chars = escapeRegExp(validChars.join(""));
+  const reg = new RegExp(`[^\\d${chars}]`, "gi");
+  return value.replace(reg, "");
+};
