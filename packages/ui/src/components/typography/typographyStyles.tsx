99629044b13d18ba261e61f4deebd6f691650c48
Corey Martin <coreyn.martin@gmail.com>
2024-11-08 10:50:47 -0800

    [private-ui] Move recaptcha to service (#13321)
    Moves recaptcha to a service for better control of script loading
    sequence and error handling. If recaptcha fails it surfaced as an error
    to the user advising them to reload the page and tracked in frontend
    logs. Instead of trying to run mutations with empty string as recaptcha
    token this will fail early and display an error to the user.
    
    We can be more clever about this in the future and try reloading the
    script a few times if we continue seeing a lot of these errors in logs.
    Let's monitor these changes first.
    
    <img width="690" alt="Screenshot 2024-11-06 at 10 48 33 PM"
    src="https://github.com/user-attachments/assets/42fdf777-3a2e-4688-8153-67b0cc07179d">
    GitOrigin-RevId: 8516671aee3369231c498f468b01ac850c461a7e

--- packages/ui/src/components/typography/typographyStyles.tsx
+++ packages/ui/src/components/typography/typographyStyles.tsx
@@ -7,6 +7,7 @@ export const typographyStyles = ({
   hideOverflow,
   displayProp,
   textAlign,
+  underline,
   onClick,
 }: { theme: Theme } & CommonStyledTypographyProps) => {
   const displayValue =
@@ -18,6 +19,10 @@ export const typographyStyles = ({
     ${typeof onClick === "function" ? "cursor: pointer;" : ""}
     ${textAlign ? `text-align: ${textAlign};` : ""}
     ${displayValue ? `display: ${displayValue};` : ""}
+
+    /* Tokens have text-decoration set, override with !important if underline specified: */
+    ${underline ? "text-decoration: underline !important;" : ""}
+
     ${hideOverflow
       ? "overflow: hidden; text-overflow: ellipsis; white-space: nowrap; max-width: 100%;"
       : ""}
