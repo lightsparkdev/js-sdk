b7f650b3dc80875760e67c6c7013d9f6dc33c5aa
Corey Martin <coreyn.martin@gmail.com>
2024-12-18 13:44:00 -0800

    [uma-bridge] Disable BridgeCardForm back button while mutations are running (#14382)
    GitOrigin-RevId: 554659a800df7ac6530938b5689912734c6d2c79

--- packages/ui/src/components/Flex.tsx
+++ packages/ui/src/components/Flex.tsx
@@ -19,8 +19,10 @@ type FlexProps = {
     | undefined;
   children?: ReactNode;
   as?: ElementType | undefined;
+  asButtonType?: "button" | "submit" | undefined;
   column?: boolean | undefined;
   onClick?: (() => void) | undefined;
+  disabled?: boolean | undefined;
   overflow?: "hidden" | "visible" | "scroll" | "auto" | undefined;
   whiteSpace?:
     | "nowrap"
@@ -43,8 +45,10 @@ export function Flex({
   column = false,
   children,
   onClick,
+  disabled = false,
   gap,
   as = "div",
+  asButtonType,
   overflow,
   whiteSpace,
   mt,
@@ -55,14 +59,16 @@ export function Flex({
   const justify = justifyProp ? justifyProp : center ? "center" : "stretch";
   const align = alignProp ? alignProp : center ? "center" : "stretch";
 
+  const asButtonProps = asButtonType ? { type: asButtonType, disabled } : {};
+
   return (
     <StyledFlex
       justify={justify}
       align={align}
       column={column}
       as={as}
-      onClick={onClick}
-      cursorProp={onClick ? "pointer" : "unset"}
+      onClick={disabled ? undefined : onClick}
+      cursorProp={onClick ? (disabled ? "not-allowed" : "pointer") : "unset"}
       overflowProp={overflow}
       whiteSpace={whiteSpace}
       mr={mr}
@@ -70,6 +76,7 @@ export function Flex({
       mt={mt}
       mb={mb}
       gap={gap}
+      {...asButtonProps}
     >
       {children}
     </StyledFlex>
@@ -80,7 +87,7 @@ type StyledFlexProps = {
   justify: NonNullable<FlexProps["justify"]>;
   align: NonNullable<FlexProps["align"]>;
   column: boolean;
-  cursorProp: "pointer" | "initial" | "unset";
+  cursorProp: "pointer" | "not-allowed" | "initial" | "unset";
   overflowProp: FlexProps["overflow"];
   whiteSpace: FlexProps["whiteSpace"];
   mt: FlexProps["mt"];
@@ -88,11 +95,26 @@ type StyledFlexProps = {
   mb: FlexProps["mb"];
   ml: FlexProps["ml"];
   gap: number | undefined;
+  as?: ElementType | undefined;
 };
 
 export const StyledFlex = styled.div<StyledFlexProps>`
   display: flex;
   text-overflow: ellipsis;
+
+  ${({ as }) =>
+    /* reset button styles */
+    as === "button" &&
+    `
+    border: 0;
+    appearance: none;
+    background: transparent;
+    padding: 0;
+    /* needed in Safari for some reason */
+    font-size: 1rem;
+    color: inherit;
+    font-weight: inherit;
+  `}
 
   ${({ column }) => column && `flex-direction: column;`}
   ${({ justify }) => `justify-content: ${justify};`}
