e625d8222a9f87a962fb9f271a53ad05f884e956
Corey Martin <coreyn.martin@gmail.com>
2025-07-14 17:54:56 -0400

    [core] Conditionally load graphql-ws client due to errors in Bare environment. Towards LIG-7942 (#19123)
    GitOrigin-RevId: 18aaebc5ab77b7ebdf51cf6091fb46de64d88d3c

--- packages/core/src/utils/environment.ts
+++ packages/core/src/utils/environment.ts
@@ -1,3 +1,7 @@
+declare global {
+  const Bare: unknown;
+}
+
 export const isBrowser =
   typeof window !== "undefined" && typeof window.document !== "undefined";
 
@@ -7,3 +11,6 @@ export const isNode =
   process.versions.node != null;
 
 export const isTest = isNode && process.env.NODE_ENV === "test";
+
+/* https://github.com/holepunchto/which-runtime/blob/main/index.js */
+export const isBare = typeof Bare !== "undefined";
