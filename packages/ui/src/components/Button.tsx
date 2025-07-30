9fbe8dd9875ec947c9b001a1ff6e4d1077132563
Brian Siao Tick Chong <bsiaotickchong@gmail.com>
2025-07-29 18:28:50 -0700

    [nage] add origin icon to header (#19504)
    ![Screenshot 2025-07-29 at 5.30.19 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/NU8OmLauzLqa61yWDJkY/a67ce2cc-5366-46e7-a490-4f415722ed0f.png)
    
    - makes icon "square" and "color" props customizable for icon
    
    GitOrigin-RevId: b85fafeb37ed2f0922b7eb8fd48f8c972feac279

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -311,8 +311,8 @@ export const Button = forwardRef<
         <Icon
           {...icon}
           width={icon.width || defaultIconWidth}
-          square
-          color={typography.color}
+          square={icon.square === undefined ? true : icon.square}
+          color={icon.color || typography.color}
         />
       </ButtonIcon>
     );
---
+++ packages/ui/src/icons/Origin.tsx
@@ -0,0 +1,23 @@
+export function Origin() {
+  return (
+    <svg
+      width="32"
+      height="20"
+      viewBox="0 0 32 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_822_14080)">
+        <path
+          d="M1.2405 17.8481L11.1639 11.4038H0L4.3237 8.59595H14.2468V2.1518L17.5603 0V7.24995L25.4108 2.15178L30.5661 2.15178L20.643 8.59595H31.8075L27.4837 11.4038H17.5603V17.8482L14.2468 20V12.7497L6.39586 17.8481H1.2405Z"
+          fill="currentColor"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_822_14080">
+          <rect width="31.8075" height="20" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
