12ff69b832356a3296059038fcc8d4ddd1efeb7c
Brian Siao Tick Chong <bsiaotickchong@gmail.com>
2025-09-08 11:57:23 -0700

    [nage] show nav items depending on user role (#20318)
    - shows nav items depending on umaaas role
    - fixes console error due to svg prop
    - removes notification bell and unused menu items
    - fixes docs link
    
    Admin and developer:
    
    ![Screenshot 2025-09-05 at 5.44.15 PM.png](https://app.graphite.dev/user-attachments/assets/701d9d7a-0cf8-4f88-9131-466d0f1a3480.png)
    
    Compliance:
    
    ![Screenshot 2025-09-05 at 5.44.25 PM.png](https://app.graphite.dev/user-attachments/assets/c87495e0-38a7-4209-8f3d-24f38a0126d5.png)
    
    Support:
    
    ![Screenshot 2025-09-05 at 5.44.03 PM.png](https://app.graphite.dev/user-attachments/assets/27b48b7f-14df-4efa-bc33-201101aa1340.png)
    
    GitOrigin-RevId: 6435f3e38ff44d0ae9f888bc9baa035a9bd9af08

--- packages/ui/src/icons/Origin.tsx
+++ packages/ui/src/icons/Origin.tsx
@@ -7,7 +7,7 @@ export function Origin() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_822_14080)">
+      <g clipPath="url(#clip0_822_14080)">
         <path
           d="M1.2405 17.8481L11.1639 11.4038H0L4.3237 8.59595H14.2468V2.1518L17.5603 0V7.24995L25.4108 2.15178L30.5661 2.15178L20.643 8.59595H31.8075L27.4837 11.4038H17.5603V17.8482L14.2468 20V12.7497L6.39586 17.8481H1.2405Z"
           fill="currentColor"
